ea50adfe89d1289a7551622bfbbca45be23a74af
0404233 <andrey0404233@gmail.com>
2022-11-17 03:32:52 +0300

    added header

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,11 @@
-//here will by JavaScript code ...
+function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+    }
+    }
+    
+    ibg();
