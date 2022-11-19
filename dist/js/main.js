19c84c257eaa1b5e6394a37ce2a9182d865456fb
0404233 <andrey0404233@gmail.com>
2022-11-19 16:18:17 +0300

    done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,65 +1,68 @@
-function ibg(){
+function ibg() {
 
-    let ibg=document.querySelectorAll(".ibg");
-    for (var i = 0; i < ibg.length; i++) {
-    if(ibg[i].querySelector('img')){
-    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
     }
+  }
+}
+
+ibg();
+
+
+// $(document).ready(function() {
+//     $('.icon-menu').click(function(event) {
+//         $('.menu__icon,.icon-menu').toggleClass('active');
+//     });
+//     $('body').toggleClass('lock')
+// });
+
+
+$('.icon-menu').click(function (event) {
+  event.stopPropagation()
+  $(this).toggleClass('active');
+  $('.menu__body').toggleClass('active');
+  $('body').toggleClass('lock');
+})
+
+
+
+// Slider
+
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     infinite: true,
+//     dots: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1
+
+//   });
+// });
+
+// Adaptive
+
+
+$('.slider').slick({
+  infinite: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  focusOnSelect: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
     }
-    }
-    
-    ibg();
-
-
-    // $(document).ready(function() {
-    //     $('.icon-menu').click(function(event) {
-    //         $('.menu__icon,.icon-menu').toggleClass('active');
-    //     });
-    //     $('body').toggleClass('lock')
-    // });
-
-
-    $('.icon-menu').click(function(event) {
-        $(this).toggleClass('active');
-        $('.menu__body').toggleClass('active');
-        $('body').toggleClass('lock');
-    })
-
-
-
-
-    // Slider
-
-    // $(document).ready(function(){
-    //   $('.your-class').slick({
-    //     infinite: true,
-    //     dots: true,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 1
-  
-    //   });
-    // });
-
-    // Adaptive
-
-
-    $('.slider').slick({
-      infinite: true,
-      dots: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-            }
-          }
-        ]
-    });
+  ]
+});
