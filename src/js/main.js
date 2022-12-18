7d1985c426e84f863492ebb536cba98f2209fc14
0404233 <andrey0404233@gmail.com>
2022-12-18 21:05:36 +0300

    gg

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,35 @@
-let login = prompt('Кто там?', '');
+// let login = prompt('Кто там?', '');
 
-if (login === 'Админ') {
-    let pass = prompt('Пароль', '');
+// if (login === 'Админ') {
+//     let pass = prompt('Пароль', '');
 
-    if (pass === 'Я главный') {
-        alert('Здравствуйте!');
-    } else if (pass === '' || pass === null) {
-        alert('Отменено');
-    } else {
-        alert('Неверный пароль');
-    }
-    
-} else if (login === '' || login === null) {
-    alert('Отменено');
-} else {
-    alert('Я вас не знаю');
-}
+//     if (pass === 'Я главный') {
+//         alert('Здравствуйте!');
+//     } else if (pass === '' || pass === null) {
+//         alert('Отменено');
+//     } else {
+//         alert('Неверный пароль');
+//     }
+
+// } else if (login === '' || login === null) {
+//     alert('Отменено');
+// } else {
+//     alert('Я вас не знаю');
+// }
+
+
+
+
+
+
+
+
+
+for (let i = 0; i < 10; i++) {
+
+    // если true, пропустить оставшуюся часть тела цикла
+    if (i % 2 == 0) continue;
+  
+    console.log(i); // 1, затем 3, 5, 7, 9
+  }
+
